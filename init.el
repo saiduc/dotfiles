db3bc3d4dc72f39c136c4dd28a385ca7aa7f1546
SaiDuc <saipandian97@gmail.com>
2019-10-30 23:02:59 +0000

    moved some parts to org file

--- init.el
+++ init.el
@@ -6,23 +6,9 @@
 ;; |_____/ \__,_|_|_____/ \__,_|\___|      github.com/saiduc
 ;; ------------------------------------------------------------------------
 
-;; SETUP PACKAGE INSTALL
-;; --------------------------------------
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-
 ;; LOAD ORG FILE
 ;; --------------------------------------
-(setq gc-cons-threshold 1000000000)
-(set-frame-font "Monaco 14" nil t)
+(package-initialize)
 
 ;; This 'compiles' the org init file so it is quicker to load
 (defun my/tangle-dotfiles ()
@@ -42,10 +28,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "35b0b0e531731e270708ddb342dc2e576a31fb298dcbc56a206596a43afac54f" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
  '(org-agenda-files (quote ("~/.emacs.d/init/init-org.org")))
  '(package-selected-packages
    (quote
-    (elpy diminish org-bullets auctex-latexmk company-auctex ein conda company-anaconda anaconda-mode pdf-tools smex exec-path-from-shell which-key all-the-icons neotree dashboard company iedit evil-commentary evil-collection autopair spaceline dracula-theme auctex auctex-lua)))
+    (company-reftex elpy diminish org-bullets auctex-latexmk company-auctex ein conda company-anaconda anaconda-mode pdf-tools smex exec-path-from-shell which-key dashboard company iedit evil-commentary evil-collection autopair spaceline dracula-theme auctex auctex-lua)))
  '(pdf-tools-handle-upgrades nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
