934feea472922403f0946d06d2a7db5e6ff20a85
SaiDuc <saipandian97@gmail.com>
2020-07-30 14:47:01 +0100

    Cosmetic changes

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -36,25 +36,4 @@
 
 ;; STUFF EMACS ADDS
 ;; --------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (org-plus-contrib autopair flycheck-cython smartparens org yasnippet-snippets which-key vterm vimrc-mode use-package smex projectile pos-tip perspective pdf-tools org-bullets neotree markdown-mode jupyter iedit flycheck exec-path-from-shell evil-surround evil-org evil-numbers evil-magit evil-commentary evil-collection esup ess elpy dracula-theme doom-modeline diminish dashboard dash-functional cython-mode csv-mode counsel conda company-reftex company-c-headers company-auctex auto-package-update auctex-latexmk)))
- '(pdf-tools-handle-upgrades nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-preview ((t (:foreground "darkgray" :underline t))))
- '(company-preview-common ((t (:inherit company-preview))))
- '(company-tooltip ((t (:background "black" :foreground "white"))))
- '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
- '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
- '(company-tooltip-selection ((t (:background "darkgray" :foreground "white"))))
- '(persp-selected-face ((t (:foreground "#FD7CC5")))))
 )
