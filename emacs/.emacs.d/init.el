d56b37b5f393bd6a5ee7ffab0abdb78bba3fd6fc
SaiDuc <saipandian97@gmail.com>
2020-08-25 16:23:30 +0100

    Moved custom to separate file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -39,34 +39,18 @@
 (load-file "~/dotfiles/emacs/.emacs.d/init-org.elc")
 
 
+;; SET CUSTOM.EL LOCATION
+;; --------------------------------------
+(setq custom-file (expand-file-name
+		   "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+
 ;; BOILERPLATE
 ;; --------------------------------------
 (add-hook 'emacs-startup-hook
     (lambda () (setq gc-cons-threshold 16777216
 		     gc-cons-percentage 0.1)))
-
-
-;; STUFF EMACS ADDS
-;; --------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (org yasnippet-snippets writeroom-mode which-key vterm vimrc-mode use-package smex projectile perspective pdf-tools org-gcal org-bullets markdown-mode jupyter iedit flycheck-cython exec-path-from-shell evil-surround evil-numbers evil-magit evil-commentary evil-collection ess elpy doom-themes doom-modeline diminish dashboard cython-mode csv-mode counsel conda company-reftex company-c-headers company-auctex autopair auto-package-update auctex-latexmk))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-document-title ((t :height 1.5)))
- '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
- '(persp-selected-face ((t (:foreground "#FD7CC5")))))
 
 )
