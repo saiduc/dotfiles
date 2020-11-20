e932562dcc1af1e664e9cedb8dd08d1ef19f70ae
SaiDuc <saipandian97@gmail.com>
2020-11-20 22:07:03 +0000

    Changed location of emacs folder in script

--- emacs/.config/emacs/oxipynb_download.sh
+++ emacs/.config/emacs/oxipynb_download.sh
@@ -1,9 +1,9 @@
-FILE=/Users/saipandian/.emacs.d/lisp/ox-ipynb.el
+FILE=~/.config/emacs/lisp/ox-ipynb.el
 if [ -f "$FILE" ]; then
     :
 else
-    mkdir /Users/saipandian/.emacs.d/lisp &&\
+	mkdir ~/.config/emacs/lisp &&\
 	git clone https://github.com/jkitchin/ox-ipynb.git &&\
-	mv ./ox-ipynb/ox-ipynb.el /Users/saipandian/.emacs.d/lisp/ &&\
+	mv ./ox-ipynb/ox-ipynb.el ~/.config/emacs/lisp/ &&\
 	rm -rf ./ox-ipynb
 fi
