9f8dc69562b1f966982ed526af61b37b460725ea
Sai Pandian <saipandian97@gmail.com>
2021-04-20 08:30:10 +0100

    Change gc cons to prevent gc on startup

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,8 +1,20 @@
 ;; disable package.el
 (setq package-enable-at-startup nil)
 
-;; set gc threshold
-(setq gc-cons-threshold 402653184)
+;; set gc threshold during normal operation
+(defvar default-gc-cons-threshold 100000000)
+
+;; make garbage collector high at startup
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; set gc cons lower again after startup
+(add-hook 'emacs-startup-hook
+          (lambda (&rest _)
+            (setq gc-cons-threshold default-gc-cons-threshold
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist default-file-name-handler-alist)
+            (makunbound 'default-file-name-handler-alist)))
 
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
 (push '(tool-bar-lines . 0) default-frame-alist)
