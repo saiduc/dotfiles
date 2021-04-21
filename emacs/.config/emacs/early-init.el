4090d91bf1dbed01896e06d68101934a4f0b9929
Sai Pandian <saipandian97@gmail.com>
2021-04-21 12:13:37 +0100

    Add startup optimisation
    + Add some more package.el suppression
    + Add some file-name-handler stuff
    + Remove maximise frame since it slows startup

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,5 +1,6 @@
 ;; disable package.el
 (setq package-enable-at-startup nil)
+(advice-add 'package--ensure-init-file :override 'ignore)
 
 ;; set gc threshold during normal operation
 (defvar default-gc-cons-threshold 100000000)
@@ -8,15 +9,22 @@
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
+;; restore gc threshold after startup
 (add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq gc-cons-threshold default-gc-cons-threshold
-                  gc-cons-percentage 0.1)))
+          (lambda () (setq gc-cons-threshold default-gc-cons-threshold
+			   gc-cons-percentage 0.1)))
 
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 
 ;; Set font
-(setq frame-inhibit-implied-resize t) 
+(setq frame-inhibit-implied-resize t)
 (add-to-list 'default-frame-alist '(font . "Roboto Mono 14"))
+
+;; optimisations
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq file-name-handler-alist default-file-name-handler-alist)) 100)
