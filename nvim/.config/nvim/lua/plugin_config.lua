5373ab3ed50589e4d6b3803676c2e49a3ee2b795
Sai Pandian <saipandian97@gmail.com>
2025-10-18 15:24:42 -0400

    :wqfix which-key warnings

--- nvim/.config/nvim/lua/plugin_config.lua
+++ nvim/.config/nvim/lua/plugin_config.lua
@@ -54,6 +54,14 @@ call wilder#set_option('pipeline', [
 ]],
 true)
 
+-- filter which-key warnings
+local orig_notify = vim.notify
+vim.notify = function(msg, level, opts)
+  if msg:match("which%-key") and level == vim.log.levels.WARN then
+    return
+  end
+  orig_notify(msg, level, opts)
+end
 require('which-key').setup{
         window = {border = 'single'},
         layout = {align = 'center',
